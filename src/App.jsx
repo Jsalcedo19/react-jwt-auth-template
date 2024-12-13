35009e0b1a6c5e6c789747352e7e82dba46d3827
Jose Salcedo <Jsalcedo19@gmail.com>
2024-12-13 11:15:03 -0500

    deleted dead code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
-im
+
 
 const App = () => {
   const [user, setUser] = useState(null)
